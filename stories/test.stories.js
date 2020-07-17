72c1391206951418ff93c805a58f21e49c2a6870
aptt <a@ptt.li>
2020-07-17 14:49:30 +0200

    feat: 🎸 add storybook config and test story with test component

---
+++ stories/test.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { TestComponent } from '../src/'
+
+export default {
+  title: 'Test'
+}
+
+export const TestComponentStory = () => <TestComponent />
+
+TestComponentStory.story = {
+  name: 'Test Component'
+}
