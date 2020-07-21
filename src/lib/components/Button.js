b1cd33b69de4277df07c64a0779c5065f14d9a78
aptt <a@ptt.li>
2020-07-21 15:19:51 +0200

    refactor(Button): 💡 add margin props

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 import button from '../mixins/button'
+import noMargin from '../mixins/noMargin'
 
 export default styled.button`
   ${button}
+  ${noMargin}
 `
