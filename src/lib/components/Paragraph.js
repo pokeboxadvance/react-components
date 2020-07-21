62078d345aad5fc77f829c231c1a3d32bcfd8398
aptt <a@ptt.li>
2020-07-21 14:24:31 +0200

    refactor(Paragraph): 💡 add fontSize prop (defaults to 'fontSizes.text')

--- src/lib/components/Paragraph.js
+++ src/lib/components/Paragraph.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
+import { fontSize } from '@themed-styling/core'
 
 import noMargin from '../mixins/noMargin'
 
 export default styled.p`
   ${noMargin}
+  ${fontSize('fontSizes.text')}
 `
