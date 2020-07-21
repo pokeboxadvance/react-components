bc16f14d747ebe5c77efb34b0494f83a705de682
aptt <a@ptt.li>
2020-07-21 13:44:32 +0200

    refactor(button-story): 💡 add round variants

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -2,19 +2,31 @@ import React from 'react'
 
 import { Button, ButtonAnchor } from '../src/'
 
+import Checkers from './util/Checkers'
+
 export default {
   title: 'Button',
 }
 
 export const ButtonStory = () => (
-  <>
+  <Checkers>
     <Button>Regular Button</Button>
     <Button inverted>Inverted Button</Button>
     <Button toggling>Regular Toggling Button</Button>
     <Button toggling inverted>
       Inverted Toggling Button
     </Button>
-  </>
+    <Button round>R</Button>
+    <Button round inverted>
+      IR
+    </Button>
+    <Button round toggling>
+      RT
+    </Button>
+    <Button round toggling inverted>
+      IRT
+    </Button>
+  </Checkers>
 )
 
 ButtonStory.story = {
