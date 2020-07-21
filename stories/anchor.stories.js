fd31d8b429ff240d1e9c3ba288f0c1852662d676
aptt <a@ptt.li>
2020-07-21 15:42:12 +0200

    refactor(anchor-stories): 💡 add app colors, checkers and round button variants

--- stories/anchor.stories.js
+++ stories/anchor.stories.js
@@ -2,21 +2,36 @@ import React from 'react'
 
 import { Anchor, ButtonAnchor } from '../src/'
 
+import Checkers from './util/Checkers'
+import AppColors from './util/AppColors'
+
 export default {
   title: 'Anchor',
 }
 
-export const AnchorStory = () => <Anchor>Anchor</Anchor>
+export const AnchorStory = () => (
+  <AppColors>
+    <Anchor style={{ display: 'inline-flex' }}>Anchor</Anchor>
+  </AppColors>
+)
 
 AnchorStory.story = {
   name: 'Anchor',
 }
 
 export const ButtonAnchorStory = () => (
-  <>
-    <ButtonAnchor>Button Anchor</ButtonAnchor>
-    <ButtonAnchor inverted>Inverted Button Anchor</ButtonAnchor>
-  </>
+  <Checkers>
+    <ButtonAnchor margin={'0.5rem'}>Button Anchor</ButtonAnchor>
+    <ButtonAnchor inverted margin={'0.5rem'}>
+      Inverted Button Anchor
+    </ButtonAnchor>
+    <ButtonAnchor round margin={'0.5rem'}>
+      RA
+    </ButtonAnchor>
+    <ButtonAnchor round inverted margin={'0.5rem'}>
+      RA
+    </ButtonAnchor>
+  </Checkers>
 )
 
 ButtonAnchorStory.story = {
