fe38244cb1ee0cfd7843a3dd7516d969f01a8378
aptt <a@ptt.li>
2020-07-22 15:34:15 +0200

    refactor(fonts): 💡 set lato as text font, remove other fonts
    remove libre baskerville and open sans

--- src/lib/GlobalFonts.js
+++ src/lib/GlobalFonts.js
@@ -2,35 +2,22 @@ import { createGlobalStyle } from 'styled-components'
 
 import { fontSize } from '@themed-styling/core'
 
-import LatoRegularWOFF from '@openfonts/lato_latin/files/lato-latin-400.woff'
-import LatoRegularWOFF2 from '@openfonts/lato_latin/files/lato-latin-400.woff2'
+import HeadingWOFF from '@openfonts/lato_latin/files/lato-latin-900.woff'
+import HeadingWOFF2 from '@openfonts/lato_latin/files/lato-latin-900.woff2'
 
-import Lato900WOFF from '@openfonts/lato_latin/files/lato-latin-900.woff'
-import Lato900WOFF2 from '@openfonts/lato_latin/files/lato-latin-900.woff2'
+import TextRegularWOFF from '@openfonts/lato_latin/files/lato-latin-400.woff'
+import TextRegularWOFF2 from '@openfonts/lato_latin/files/lato-latin-400.woff2'
 
-import LibreBaskervilleRegularWOFF from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-400.woff'
-import LibreBaskervilleRegularWOFF2 from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-400.woff2'
+import TextItalicWOFF from '@openfonts/lato_latin/files/lato-latin-400-italic.woff'
+import TextItalicWOFF2 from '@openfonts/lato_latin/files/lato-latin-400-italic.woff2'
 
-import LibreBaskervilleRegularItalicWOFF from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-400-italic.woff'
-import LibreBaskervilleRegularItalicWOFF2 from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-400-italic.woff2'
-
-import LibreBaskervilleBoldWOFF from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-700.woff'
-import LibreBaskervilleBoldWOFF2 from '@openfonts/libre-baskerville_latin/files/libre-baskerville-latin-700.woff2'
+import TextBoldWOFF from '@openfonts/lato_latin/files/lato-latin-700.woff'
+import TextBoldWOFF2 from '@openfonts/lato_latin/files/lato-latin-700.woff2'
 
 // TODO: take font family from theme
 
 export default createGlobalStyle`
-  @font-face {
-    font-family: 'Lato';
-    font-style: normal;
-    font-display: swap;
-    font-weight: 400;
-    src:
-      local('Lato Regular'),
-      local('Lato-Regular'),
-      url(${LatoRegularWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-      url(${LatoRegularWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-  }
+  /* HEADING */
 
   @font-face {
     font-family: 'Lato';
@@ -40,48 +27,52 @@ export default createGlobalStyle`
     src:
       local('Lato Black'),
       local('Lato-Black'),
-      url(${Lato900WOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-      url(${Lato900WOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      url(${HeadingWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url(${HeadingWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
 
+  /* TEXT REGULAR */
   @font-face {
-    font-family: 'Libre Baskerville';
+    font-family: 'Lato';
     font-style: normal;
     font-display: swap;
     font-weight: 400;
     src:
-      local('Libre Baskerville'),
-      local('LibreBaskerville-Regular'),
-      url(${LibreBaskervilleRegularWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-      url(${LibreBaskervilleRegularWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      local('Lato Regular'),
+      local('Lato-Regular'),
+      url(${TextRegularWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url(${TextRegularWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
 
+  /* TEXT ITALIC */
   @font-face {
-    font-family: 'Libre Baskerville';
+    font-family: 'Lato';
     font-style: italic;
     font-display: swap;
     font-weight: 400;
     src:
-      local('Libre Baskerville Italic'),
-      local('LibreBaskerville-Italic'),
-      url(${LibreBaskervilleRegularItalicWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-      url(${LibreBaskervilleRegularItalicWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      local('Lato Italic'),
+      local('Lato-Italic'),
+      url(${TextItalicWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url(${TextItalicWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
 
+  /* TEXT BOLD */
   @font-face {
-    font-family: 'Libre Baskerville';
+    font-family: 'Lato';
     font-style: normal;
     font-display: swap;
     font-weight: 700;
     src:
-      local('Libre Baskerville Bold'),
-      local('LibreBaskerville-Bold'),
-      url(${LibreBaskervilleBoldWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
-      url(${LibreBaskervilleBoldWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      local('Lato Bold'),
+      local('Lato-Bold'),
+      url(${TextBoldWOFF2}) format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+      url(${TextBoldWOFF}) format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
   }
 
   html {
-    font-family: Libre Baskerville, serif;
+    font-family: Lato, sans-serif;
+    font-weight: 500;
     word-spacing: 0.1em;
     line-height: 2;
     ${fontSize('fontSizes.root')}
